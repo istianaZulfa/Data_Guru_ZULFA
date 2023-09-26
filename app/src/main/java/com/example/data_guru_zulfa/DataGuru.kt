2c85cca3847407fe040232e45a17f5f1cf82dfdd
istiana zulfa <zulfangaluran@gmail.com>
2023-09-26 15:37:24 +0700

    Initial commit

---
+++ app/src/main/java/com/example/data_guru_zulfa/DataGuru.kt
@@ -0,0 +1,11 @@
+package com.example.data_guru_zulfa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DataGuru : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_data_guru)
+    }
+}
